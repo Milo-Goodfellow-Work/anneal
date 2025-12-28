b2ed9f57b2f3c2b4ea6a43e41f5db56dd5877ae4
Odds <milogoodfellowworkemail@gmail.com>
2025-12-28 00:30:22 +0000

    Incomplete setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+'''
+
+'''
+
+import subprocess, sys, os
+
+def rust_lean(src):
+    # Get name of crate root dir
+    crate_name = os.path.basename(os.path.normpath(src))
+    # And relative path of Charon gen'd llbc
+    llbc_path = os.path.join(src, crate_name + '.llbc')
+
+    # Create Lake Lean project
+    lean_dir = os.path.join(os.path.dirname(os.path.abspath(src)), 'spec')
+    subprocess.run(['lake', 'new', 'spec', 'math'], cwd=os.path.dirname(lean_dir), check=True)
+
+    # Generate llbc file
+    subprocess.run(['charon', 'cargo', '--preset=aeneas'], cwd=src, check=True)
+
+    # Generate Lean
+    subprocess.run(['aeneas', '-backend', 'lean', '-dest', lean_dir, llbc_path], cwd=src, check=True)
+
+    # Convert from snake case to camel to get generated Lean file name
+    lean_name = ''.join(part.capitalize() for part in crate_name.split('_'))
+
+    # Get generated Lean
+    return open(f'{lean_dir}/{lean_name}.lean', 'r').read()
+
+src = os.path.expanduser(sys.argv[1])
+#gend_file = open('Generated.lean', 'w+')
+print(rust_lean(src))
