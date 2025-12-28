79e1bb673354489082da84410b142253459147ad
Odds <milogoodfellowworkemail@gmail.com>
2025-12-28 22:58:35 +0000

    Working initial test Lean parse into project

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 '''
-
 '''
 
 import subprocess, sys, os
@@ -7,24 +6,30 @@ import subprocess, sys, os
 def rust_lean(src):
     # Get name of crate root dir
     crate_name = os.path.basename(os.path.normpath(src))
-    # And relative path of Charon gen'd llbc
-    llbc_path = os.path.join(src, crate_name + '.llbc')
+    # Path of Charon gen'd llbc (relative to cwd=src)
+    llbc_path = crate_name + '.llbc'
 
-    # Create Lake Lean project
-    lean_dir = os.path.join(os.path.dirname(os.path.abspath(src)), 'spec')
+    # Use existing spec Lake project next to src, and generate directly into its Lean lib dir
+    lean_proj = os.path.join(os.path.dirname(os.path.abspath(src)), 'spec')
+    lean_dir = os.path.join(lean_proj, 'Spec')
 
     # Generate llbc file
     subprocess.run(['charon', 'cargo', '--preset=aeneas'], cwd=src, check=True)
 
-    # Generate Lean
+    # Generate Lean directly into spec/Spec
     subprocess.run(['aeneas', '-backend', 'lean', '-dest', lean_dir, llbc_path], cwd=src, check=True)
 
     # Convert from snake case to camel to get generated Lean file name
-    lean_name = ''.join(part.capitalize() for part in crate_name.split('_'))
+    if '_' in crate_name:
+        lean_name = ''.join(part.capitalize() for part in crate_name.split('_'))
+    else:
+        lean_name = crate_name
+
+    # Add generated module to the Spec project
+    with open(os.path.join(lean_proj, 'Spec.lean'), 'a') as f:
+        f.write(f'\nimport Spec.{lean_name}\n')
 
     # Get generated Lean
     return open(f'{lean_dir}/{lean_name}.lean', 'r').read()
 
-# src = os.path.expanduser(sys.argv[1])
-# gend_file = open('Generated.lean', 'w+')
 print(rust_lean("./TestProject"))
--- spec/Spec.lean
+++ spec/Spec.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `Spec` library.
 -- Import modules here that should be built as part of the library.
 import Spec.Basic
+
+import Spec.TestProject
