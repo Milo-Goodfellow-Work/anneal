6b9a1d80a513d7279541949212c02d54e995c841
Odds <milogoodfellowworkemail@gmail.com>
2025-12-28 22:42:47 +0000

    Empty spec project creating Dockerfile

---
+++ spec/Main.lean
@@ -0,0 +1,4 @@
+import Spec
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ spec/Spec.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Spec` library.
+-- Import modules here that should be built as part of the library.
+import Spec.Basic
