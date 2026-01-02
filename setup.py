8a32de221fd56494c727f29184d359d956679ed1
Odds <milogoodfellowworkemail@gmail.com>
2026-01-02 01:32:48 +0000

    Full Docker container, more or less complete basic parse, basic tests

--- setup.py
+++ setup.py
@@ -11,6 +11,21 @@ def rust_lean(src):
     # Generate directly into the Spec library folder
     lean_dir = os.path.join(spec_root, 'Spec')
 
+    # Remove Basic.lean if it exists
+    basic_lean = os.path.join(lean_dir, "Basic.lean")
+    if os.path.exists(basic_lean):
+        os.remove(basic_lean)
+
+    # Remove Main.lean if it exists (Library-only build)
+    main_lean = os.path.join(spec_root, "Main.lean")
+    if os.path.exists(main_lean):
+        os.remove(main_lean)
+
+    # Wipe Spec.lean clean so it starts empty
+    with open(os.path.join(spec_root, 'Spec.lean'), 'w') as f:
+        f.write("-- This module serves as the root of the `Spec` library.\n")
+        f.write("-- Import modules here that should be built as part of the library.\n\n")
+
     # Generate llbc file
     subprocess.run(['charon', 'cargo', '--preset=aeneas'], cwd=src, check=True)
 
@@ -20,11 +35,9 @@ def rust_lean(src):
     # Convert from snake case to camel to get generated Lean file name / module name
     lean_name = ''.join(part.capitalize() for part in crate_name.split('_'))
 
-    # Append import into the Spec root module (NO checks)
+    # Append import into the Spec root module
     with open(os.path.join(spec_root, 'Spec.lean'), 'a') as f:
         f.write(f"import Spec.{lean_name}\n")
 
     # Return generated Lean
     return open(os.path.join(lean_dir, f"{lean_name}.lean"), 'r').read()
-
-print(rust_lean("./TestProject"))
--- spec/Main.lean
+++ spec/Main.lean
@@ -1,4 +1,3 @@
 import Spec
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def main : IO Unit := pure ()
